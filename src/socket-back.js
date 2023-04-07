6aaf3481d72536a1542a893628d652945710a555
Rafael <velofuri@gmail.com>
2023-04-07 13:16:17 -0300

    Evento criado e dividido em arquivos cada qual com sua responsabilidade

---
+++ src/socket-back.js
@@ -0,0 +1,15 @@
+import io from "./servidor.js";
+
+
+io.on("connection", (socket) => {
+  console.log("um cliente se conectou! ID:", socket.id);
+
+  socket.on("texto_editor", (texto) => {
+    socket.broadcast.emit("texto_editor_clientes", texto)
+  })
+
+  socket.on("disconnect", (motivo) => {
+    console.log(`Cliente "${socket.id}" desconectado!
+    Motivo: ${motivo}`);
+  });
+});
